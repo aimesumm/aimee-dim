8ab6afdeb5cb639a39c11162848690e9671e0b99
aimesumm <aime.summ@gmail.com>
2026-07-26 01:08:45 +0800

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
-import { useEffect } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import MenuPage from './pages/MenuPage'
+import OriginalVariantPage from './pages/OriginalVariantPage'
 import OrderPage from './pages/OrderPage'
 import PaymentPage from './pages/PaymentPage'
 import QrisSuccessPage from './pages/QrisSuccessPage'
@@ -9,53 +9,15 @@ import CashSuccessPage from './pages/CashSuccessPage'
 import PaymentFailedPage from './pages/PaymentFailedPage'
 import { CartProvider } from './context/CartContext'
 import { OrderDraftProvider } from './context/OrderDraftContext'
-import StarsBackground from './components/StarsBackground'
 
 export default function App() {
-  useEffect(() => {
-    let lenisInstance = null
-    let frameId = 0
-    let cancelled = false
-
-    const start = async () => {
-      try {
-        const { default: Lenis } = await import('lenis')
-        if (cancelled) return
-
-        lenisInstance = new Lenis({
-          smoothWheel: true,
-          smoothTouch: true,
-          duration: 1.05,
-          easing: (t) => 1 - Math.pow(1 - t, 3),
-        })
-
-        const raf = (time) => {
-          lenisInstance?.raf(time)
-          frameId = window.requestAnimationFrame(raf)
-        }
-
-        frameId = window.requestAnimationFrame(raf)
-      } catch {
-        // Lenis is optional; native scroll stays as fallback.
-      }
-    }
-
-    start()
-
-    return () => {
-      cancelled = true
-      if (frameId) window.cancelAnimationFrame(frameId)
-      if (lenisInstance) lenisInstance.destroy()
-    }
-  }, [])
-
   return (
     <CartProvider>
       <OrderDraftProvider>
-        <StarsBackground />
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<MenuPage />} />
+            <Route path="/variant/:id" element={<OriginalVariantPage />} />
             <Route path="/order" element={<OrderPage />} />
             <Route path="/checkout" element={<Navigate to="/order" replace />} />
             <Route path="/payment/:method/:orderId" element={<PaymentPage />} />
